c07b1496e73872c5f3c5adbc2e12ad2c4d6917d6
o123444ya <57318786+o123444ya@users.noreply.github.com>
2021-12-17 18:57:48 +0800

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,51 +1,42 @@
-import requests, json, os, time
+import requests, json, os, time, cloudscraper
 #Made By HuanXiang
 #Https://fb.com/o123444ya
 class mail:
 	def __init__(self):
-		self.cookies=None
-		self.address=None
-	def getmail(self):
-		req=requests.get('https://10minutemail.com/session/address')
-		if req.status_code != 200:return 'error'
-		else:
-			try:
-				address=json.loads(req.text)['address']
-				self.cookies=req.cookies
-				self.address=address
-				return f'此次mail為{address}'
-			except Exception as e:return str(e)
-	def checkexpired(self):
-		req=requests.get('https://10minutemail.com/session/expired',cookies=self.cookies)
-		if json.loads(req.text)['expired']==True:
+		self.Request=cloudscraper.create_scraper()
+	def CheckExpired(self):
+		req_=self.Request.get('https://10minutemail.com/session/secondsLeft')
+		if json.loads(req_.text)['secondsLeft']==0:
 			return True
-		else:return False
-	def getmessage(self):
-		if self.cookies != None:
-			if self.address != None:
-				try:
-					if self.checkexpired() == False:
-						req=requests.get('https://10minutemail.com/messages/messagesAfter/0',cookies=self.cookies)
-						if req.status_code!=200:return 'error'
-						if req.text == '[]':return 'N'
-						else:
-							reqjs=json.loads(req.text[1:][:-1])
-							txt='收信結果'
-							txt+=f"\n發送者:{reqjs['sender']}"
-							txt+=f"\n時間:{reqjs['sentDate']}"
-							txt+=f"\n主旨:{reqjs['subject']}"
-							txt+=f"\n內容:{reqjs['bodyPreview']}"
-							return txt
-					else:return 'expired'
-				except Exception as e:return str(e)
+		else:
+			return False
+	def GetMail(self):
+		req_=self.Request.get('https://10minutemail.com/session/address')
+		self.Request.cookies=req_.cookies
+		return json.loads(req_.text)['address']
+	def GetMessage(self):
+		if not self.CheckExpired():
+			req_=self.Request.get('https://10minutemail.com/messages/messagesAfter/0')
+			if req_.status_code!=200:return"Error"
+			elif req_.text=='[]':return"N"
+			else:
+				reqjs=json.loads(req_.text[1:][:-1])
+				txt='收信結果'
+				txt+=f"\n發送者:{reqjs['sender']}"
+				txt+=f"\n時間:{reqjs['sentDate']}"
+				txt+=f"\n主旨:{reqjs['subject']}"
+				txt+=f"\n內容:{reqjs['bodyPreview']}"
+				return txt
 print('歡迎使用幻想信箱py\n請勿作為商業用途')
 mail=mail()
-print(mail.getmail())
+print('取得信箱中...')
+print(mail.GetMail())
+print('取得成功\n正在檢測收信...')
 while True:
 	try:
-		get=mail.getmessage()
-		if get =='expired':print('過時');break
-		if get =='error':print('非200狀態錯誤');break
+		get=mail.GetMessage()
+		if get =='expired':print('過時\n請重新取得');break
+		if get =='error':print('非200狀態錯誤\n請查看是否被封鎖IP\n如無法請聯繫幻想\nhttps://fb.com/o123444ya');break
 		if get =='N':pass
 		else:print(get);break
 		time.sleep(5)#anti ban
