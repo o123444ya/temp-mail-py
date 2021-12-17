de5f710fb7bf8ed24e4d184bddf2073da39f257f
o123444ya <57318786+o123444ya@users.noreply.github.com>
2021-12-17 22:13:20 +0800

    美化了一下<3

--- mail.py
+++ mail.py
@@ -1,15 +1,24 @@
-import requests, json, os, time, cloudscraper
+import json, time, cloudscraper, sys, threading, os
 #Made By HuanXiang
 #Https://fb.com/o123444ya
 class mail:
 	def __init__(self):
 		self.Request=cloudscraper.create_scraper()
+		self.IsReceiving=True
 	def CheckExpired(self):
 		req_=self.Request.get('https://10minutemail.com/session/secondsLeft')
 		if json.loads(req_.text)['secondsLeft']==0:
 			return True
 		else:
 			return False
+	def RemainingTime(self):
+		try:
+			for Time in range(600):
+				if self.IsReceiving:
+					sys.stdout.write(f'\r[ 剩餘時間 ] {600-Time} 秒')
+					sys.stdout.flush()
+					time.sleep(1)
+		except:return
 	def GetMail(self):
 		req_=self.Request.get('https://10minutemail.com/session/address')
 		self.Request.cookies=req_.cookies
@@ -17,28 +26,30 @@ class mail:
 	def GetMessage(self):
 		if not self.CheckExpired():
 			req_=self.Request.get('https://10minutemail.com/messages/messagesAfter/0')
-			if req_.status_code!=200:return"Error"
+			if req_.status_code!=200:self.IsReceiving=False;return"Error"
 			elif req_.text=='[]':return"N"
 			else:
 				reqjs=json.loads(req_.text[1:][:-1])
-				txt='收信結果'
+				txt='\n收信結果'
 				txt+=f"\n發送者:{reqjs['sender']}"
 				txt+=f"\n時間:{reqjs['sentDate']}"
 				txt+=f"\n主旨:{reqjs['subject']}"
 				txt+=f"\n內容:{reqjs['bodyPreview']}"
+				self.IsReceiving=False
 				return txt
 print('歡迎使用幻想信箱py\n請勿作為商業用途')
 mail=mail()
 print('取得信箱中...')
 print(mail.GetMail())
 print('取得成功\n正在檢測收信...')
+threading.Thread(target=mail.RemainingTime).start()
 while True:
 	try:
 		get=mail.GetMessage()
-		if get =='expired':print('過時\n請重新取得');break
-		if get =='error':print('非200狀態錯誤\n請查看是否被封鎖IP\n如無法請聯繫幻想\nhttps://fb.com/o123444ya');break
+		if get =='expired':print('\n過時\n請重新取得');break
+		if get =='error':print('\n非200狀態錯誤\n請查看是否被封鎖IP\n如無法請聯繫幻想\nhttps://fb.com/o123444ya');break
 		if get =='N':pass
 		else:print(get);break
 		time.sleep(5)#anti ban
-	except KeyboardInterrupt:print('Ctrl-C');os._exit(1)
+	except KeyboardInterrupt:print('\nCtrl-C');os._exit(1)
 	except Exception as e:print(str(e))
